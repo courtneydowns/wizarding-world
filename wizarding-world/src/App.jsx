b2d94fc24ba62541b2271ffa4a9c4f17c48b23a4
Chase Allen <ccallen001@gmail.com>
2021-09-30 13:31:31 -0400

    put the spells in an array

--- wizarding-world/src/App.jsx
+++ wizarding-world/src/App.jsx
@@ -1,26 +1,35 @@
-import React, { useState, useEffect } from "react";
-import { getDatabase, ref, onValue } from "firebase/database";
-import "./App.css";
+import React, { useState, useEffect } from 'react';
+import { getDatabase, ref, onValue } from 'firebase/database';
+import './App.css';
 
 function App() {
   const database = getDatabase();
-  const spellsRef = ref(database, "spells");
+  const spellsRef = ref(database, 'spells');
 
   const [spells, setSpells] = useState([]);
 
   useEffect(() => {
     onValue(spellsRef, (snapshot) => {
       const val = snapshot.val();
-      console.log(val);
-      setSpells(val);
+
+      const spellsArr = [];
+
+      for (const spell in val) {
+        spellsArr.push({ ...val[spell] });
+      }
+
+      setSpells(spellsArr);
     });
   });
+
   return (
     <div>
       <h3>Spells</h3>
       <ul>
         {spells.map((spell) => (
-          <li key={spell.name}>{spell.name}</li>
+          <li key={spell.Name}>
+            <span>Name: {spell.Name}</span>
+          </li>
         ))}
       </ul>
     </div>
