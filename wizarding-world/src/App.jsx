e09595d670c260b2fbfcd49798f2f29035fbbc26
courtneydowns <84096318+courtneydowns@users.noreply.github.com>
2021-10-05 11:33:47 -0400

    Deleted server side files that were not needed and added code to app.jsx to get spell names listed with properties

--- wizarding-world/src/App.jsx
+++ wizarding-world/src/App.jsx
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import { getDatabase, ref, onValue } from 'firebase/database';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { getDatabase, ref, onValue, child } from "firebase/database";
+import "./App.css";
 
 function App() {
   const database = getDatabase();
-  const spellsRef = ref(database, 'spells');
+  const spellsRef = ref(database, "spells");
 
   const [spells, setSpells] = useState([]);
 
@@ -22,6 +22,19 @@ function App() {
     });
   });
 
+  const dbRef = ref(getDatabase());
+  get(child(dbRef, `spells/${spell.Name}`))
+    .then((snapshot) => {
+      if (snapshot.exists()) {
+        console.log(snapshot.val());
+      } else {
+        console.log("No data available");
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+
   return (
     <div>
       <h3>Spells</h3>
@@ -29,6 +42,21 @@ function App() {
         {spells.map((spell) => (
           <li key={spell.Name}>
             <span>Name: {spell.Name}</span>
+            <li key={spell.Type}>
+              <span>Type: {spell.Type}</span>
+              <li key={spell.Pronunciation}>
+                <span>Pronunciation: {spell.Pronunciation}</span>
+                <li key={spell.Use}>
+                  <span>Use: {spell.Use}</span>
+                  <li key={spell.Etymology}>
+                    <span>Etymology: {spell.Etymology}</span>
+                    <li key={spell.Appearances}>
+                      <span>Appearances: {spell.Appearances}</span>
+                    </li>
+                  </li>
+                </li>
+              </li>
+            </li>
           </li>
         ))}
       </ul>
